f0e5a8081fe8fa6a4c67a35d933a9bf67e9aceb3
ErenYagci <yagcieren2006@gmail.com>
2026-09-24 00:46:08 +0300

    Voltage divider updated

--- Voltage_Divider.c
+++ Voltage_Divider.c
@@ -2,9 +2,9 @@
 
 // Voltage Divider: V_out = V_in * (R2 / (R1 + R2))
 int main() {
-    double v_in = 15.0; // Input Voltage [V]
-    double r1 = 1000.0; // Resistor 1 [Ohm]
-    double r2 = 2000.0; // Resistor 2 [Ohm]
+    double v_in = 15.0; 
+    double r1 = 1000.0; 
+    double r2 = 2000.0; 
 
     double v_out = v_in * (r2 / (r1 + r2));
 
